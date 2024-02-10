9ad1639c08a2c325c368fdb7ece8a62b7aad095c
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2024-02-10 22:59:58 +0000

    words

---
+++ words.py
@@ -0,0 +1,12 @@
+words = [
+    "apple", "banana", "orange", "grape", "pineapple", 
+    "strawberry", "kiwi", "watermelon", "peach", "mango", 
+    "blueberry", "apricot", "blackberry", "cherry", "coconut", 
+    "fig", "lemon", "lime", "papaya", "pear", 
+    "plum", "raspberry", "tangerine", "avocado", "cranberry", 
+    "elderberry", "guava", "lychee", "melon", "nectarine", 
+    "passionfruit", "pomegranate", "dragonfruit", "rhubarb", "starfruit", 
+    "boysenberry", "persimmon", "plantain", "soursop", "kumquat", 
+    "date", "quince", "mulberry", "durian", "breadfruit", 
+    "cantaloupe", "honeydew", "clementine", "lychee"
+]
